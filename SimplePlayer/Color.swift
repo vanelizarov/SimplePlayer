4a790bab128d5dadfca972b458b732ea889d62d2
vanelizarov <elizarov.vanya@gmail.com>
2018-01-29 05:55:41 +0300

    Described default color palette

---
+++ SimplePlayer/Color.swift
@@ -0,0 +1,45 @@
+//
+//  Color.swift
+//  SimplePlayer
+//
+//  Created by vanya elizarov on 28/01/18.
+//  Copyright © 2018 vanya elizarov. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    static var primaryDark: UIColor {
+        return UIColor(red:0.19, green:0.25, blue:0.62, alpha:1.0)
+    }
+    
+    static var primaryLight: UIColor {
+        return UIColor(red:0.77, green:0.79, blue:0.91, alpha:1.0)
+    }
+    
+    static var primary: UIColor {
+        return UIColor(red:0.25, green:0.32, blue:0.71, alpha:1.0)
+    }
+    
+    static var accent: UIColor {
+        return UIColor(red:1.00, green:0.25, blue:0.51, alpha:1.0)
+    }
+    
+    static var textPrimary: UIColor {
+        return UIColor(red:0.13, green:0.13, blue:0.13, alpha:1.0)
+    }
+    
+    static var textSecondary: UIColor {
+        return UIColor(red:0.46, green:0.46, blue:0.46, alpha:1.0)
+    }
+    
+    static var divider: UIColor {
+        return UIColor(red:0.74, green:0.74, blue:0.74, alpha:1.0)
+    }
+    
+    static var semiBlack: UIColor {
+        return UIColor(red:0.07, green:0.07, blue:0.07, alpha:1.0)
+    }
+}
